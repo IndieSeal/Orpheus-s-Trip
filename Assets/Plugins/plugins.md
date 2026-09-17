26eb87c603b135e750e852b7f583d56cdd5e9d1e
Indie Seal <alxoanimations@gmail.com>
2026-09-16 17:59:16 -0700

    Change to 3D and Orpheus Sprite!
    I made a lil' pirate orpheus :D!
    
    Also I changed to 3D, because I got inspiration from the Out-To-C website, so I want to try to replicate that style.

---
+++ Assets/Plugins/plugins.md
@@ -0,0 +1,2 @@
+https://assetstore.unity.com/packages/tools/particles-effects/lowpoly-water-107563
+https://assetstore.unity.com/packages/2d/textures-materials/sky/skybox-series-free-103633
---
+++ Assets/Scriptables/Enemies/EnemySO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ExampleEnemy", menuName = "Indie Seal/Scriptables/EnemySO")]
+public class EnemySO : ScriptableObject
+{
+    public string eName = "Example Enemy";
+    public Sprite mainSprite;
+}
